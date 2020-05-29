4e55deadb2ce7b8aeaf6e4e016e9027a882b602f
gravve <gravve@users.noreply.github.com>
2020-05-29 17:13:35 +0300

    v0.0.1

--- Assets/Scripts/CameraMove.cs
+++ Assets/Scripts/CameraMove.cs
@@ -1,14 +1,24 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class CameraMove : MonoBehaviour
 {
 
-    public GameObject player;
+    public Transform player;
     public PlayerMove playerMovement;
    
     private float cameraDistance = 90.0f;
+    public float smoothTime = .15f;
+    Vector3 velocity = Vector3.zero;
+
+    public float yMaxValue = 0;
+    public bool yMaxEnable = false;
+
+    public float yMinValue = 0;
+    public bool yMinEnable = false;
 
 
     void Awake()
@@ -18,6 +28,26 @@ public class CameraMove : MonoBehaviour
     
     void FixedUpdate()
     {
-        transform.position = new Vector3(player.transform.position.x, player.transform.position.y + player.transform.localScale.y / 2.7f, -350f);
+        Vector3 playerPos = player.position;
+
+        if(yMaxEnable && yMaxEnable)
+        {
+            playerPos.y = Mathf.Clamp(player.position.y, yMinValue, yMaxValue);
+        }
+        else if (yMaxEnable)
+        {
+            playerPos.y = Mathf.Clamp(player.position.y , player.position.y, yMaxValue);
+        }
+        else if(yMinEnable)
+        {
+            playerPos.y = Mathf.Clamp(player.position.y, yMinValue, player.position.y);
+        }
+
+
+        playerPos.z = transform.position.z;
+
+        transform.position = Vector3.SmoothDamp(transform.position, playerPos, ref velocity, smoothTime);
+        
+        
     }
 }
