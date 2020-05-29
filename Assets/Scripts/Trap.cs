b3cbaa2f9cd556e1affcf23ff45a434064c5d4dc
gravve <gravve@users.noreply.github.com>
2020-05-29 18:06:18 +0300

    TRAPS

---
+++ Assets/Scripts/Trap.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trap : MonoBehaviour
+{
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        
+        if(other.GetComponent<HealtBar>())
+        {
+            HealtBar damage = other.GetComponent<HealtBar>();
+            damage.GiveDamage(2);
+            
+        }
+    }
+}
